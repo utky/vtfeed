723860a39ff2b15bdb37c4e2b283e2c3dc05cdd4
utky <ilyaletre@gmail.com>
2018-06-24 11:38:54 +0000

    Add redirection to index.html

--- src/vtfeed/handler/static.clj
+++ src/vtfeed/handler/static.clj
@@ -1,29 +1,8 @@
 (ns vtfeed.handler.static
   (:require [ataraxy.core :as ataraxy]
-            [ataraxy.response :as response] 
-            [ring.util.response :as ring]
+            [ataraxy.response :as response]
             [integrant.core :as ig]))
-
-(def ^:private body-template
-  "<!DOCTYPE html>
-<html>
-<head>
-<title>Hello World</title>
-  <meta charset=\"utf-8\">
-<meta name=\"description\" content=\"\">
-<meta name=\"author\" content=\"\">
-<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">
-<link rel=\"stylesheet\" href=\"\">
-<link rel=\"shortcut icon\" href=\"\">
-</head>
-<body>
-<h1>Hello World</h1>
-</body>
-</html>")
 
 (defmethod ig/init-key :vtfeed.handler.static/index [_ options]
   (fn [{[_] :ataraxy/result}]
-    (-> {:status 200
-         :body body-template}
-        (ring/content-type "text/html")
-        (ring/charset "utf8"))))
+    [::response/found "/index.html"]))
